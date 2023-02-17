a7ccd7377350a5889d6a4cc1cd6bb82c7d30d39b
ashishpandey2600 <ashishpandey@Ashishs-MacBook-Pro.local>
2023-02-17 15:22:41 +0530

    Some Files

---
+++ codes_java/Lab 10/q1.java
@@ -0,0 +1,28 @@
+package Lab 10;
+
+class second implements{
+    public int x;
+    public second (int x);
+    {
+        this.x=x;
+
+    }
+    public void run()
+    {
+        system.out.println("Second thread: Square of the numbert is "+x*x);
+
+    }
+}
+class third implements Runnable{
+    public third(int x){
+        this.x=x;
+
+    }
+    public void run()
+    {
+        Sys
+    }
+}
+
+
+}
---
+++ lab13/q1.java
@@ -0,0 +1,20 @@
+public class q1 {
+    public void main(String[] args)
+    {
+        printTheory(()->system.out.println("Hello"));
+
+    }
+}
+class main{
+    main(){
+      printTheory(()-> system.out.println("Hello"));
+    }
+}
+static void checknumber(NumberChecker n){
+    if(check==1)
+    system.our.println("node"+n.check(25));
+    if(check==2)
+    system.our.println("node"+n.check(25));
+    if(check==3)
+    system.our.println("node"+n.check(25));
+}
